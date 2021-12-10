8985e50b1790484104ceda7c7303aa79a3d8d4c3
thrombe <yash.n.thombare620@gmail.com>
2021-12-10 21:00:08 +0530

    added luunix condition

--- src/cui_handle.py
+++ src/cui_handle.py
@@ -3,8 +3,10 @@ import logging
 import enum
 
 import py_cui
-import ueberzug.lib.v0 as ueberzug
 import time
+import platform
+LUUNIX = platform.system() == "Linux"
+if LUUNIX: import ueberzug.lib.v0 as ueberzug
 from wcwidth import wcwidth, wcswidth
 
 import pydub
@@ -25,7 +27,7 @@ def pad(string):
         pads += p
     # print(len(string+zwsp*pads))
     # if pads != 0: print(len(string+zwsp*pads))#################
-    return string[:1] + zwsp*pads
+    return string + zwsp*pads
 
 class CUI_handle:
     def __init__(self):
@@ -48,6 +50,7 @@ class CUI_handle:
             # picui.set_border_color()
             # picui.set_focus_border_color()
         # TODO: set quit, pause, seek, ... as global shortcuts (i.e, set them on every widget + master)
+        # TODO: select browser widget by default
 
         self.player_widget = PlayerWidget(self.pycui.add_scroll_menu('Player', 0, 1, row_span=1, column_span=1, padx = 1, pady = 0))
         self.browser_widget = BrowserWidget(
@@ -61,17 +64,17 @@ class CUI_handle:
     def start(self):
         if getattr(self, "pycui", None) is None: self.setup()
 
-        if True: # add condition for linux here
+        if LUUNIX: # add condition for linux here
             with ueberzug.Canvas() as canvas:
                 # TODO: crop the pic to be square so things are predictable (album art is generally square too)
                 self.player_widget.image_placement = canvas.create_placement('album_art', scaler=ueberzug.ScalerOption.FIT_CONTAIN.value)
                 self.player_widget.image_placement.visibility = ueberzug.Visibility.INVISIBLE
                 self.pycui.start()
         else:
-            self.picui.start()
+            self.pycui.start()
 
     def refresh(self):
-        self.player_widget.refresh()
+        if LUUNIX: self.player_widget.refresh()
 
 class Player:
     def __init__(self):
@@ -139,7 +142,7 @@ class PlayerWidget:
 
     def play(self, song):
         self.player.play(song)
-        self.replace_album_art(song)
+        if LUUNIX: self.replace_album_art(song)
         self.print_song_metadata(song)
 
     def replace_album_art(self, song):
