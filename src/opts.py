9a4d09d01eaf3f123249a75c0140304f4b3e0462
thrombe <yash.n.thombare620@gmail.com>
2022-01-21 04:59:13 +0530

    now using toml for options

--- src/opts.py
+++ src/opts.py
@@ -1,32 +1,63 @@
 
+import toml
+import os
 
-# PATHS WITH SEPERATORS(at end) PLZ
+default_opts_path = "./db/default_opts.toml"
+opts_path = "./db/opts.toml"
 
-musi_path = "/home/issac/daata/phon-data/.musi/IsBac/" # path where music should be stored and stuff
-auto_search_under = "/home/issac/daata/phon-data/.musi/"
-file_explorer_base_dir = "/home/issac/"
-musimanager_directory = "/home/issac/0Git/musimanager/db/" # directory where musimanager can store files
+if os.path.exists(default_opts_path):
+    with open(default_opts_path, "r") as f:
+        opts = toml.load(f)
+else:
+    print("default_options file missing")
+
+if os.path.exists(opts_path):
+    with open(opts_path, "r") as f:
+        opts1 = toml.load(f)
+    for k1, v1 in opts.items():
+        for k2, v2 in opts1.items():
+            if k1 == k2:
+                opts[k1].update(opts1[k1])
+                break
+
+for k, p in opts["paths"].items():
+    opts["paths"][k] = os.path.expanduser(p)
+    if not p.endswith("/"):
+        opts["paths"][k] += "/"
+
+# import json
+# print(json.dumps(opts, indent=4))
+
+musi_path = opts["paths"]["musi_path"]
+auto_search_under = opts["paths"]["auto_search_under"]
+file_explorer_base_dir = opts["paths"]["file_explorer_base_dir"]
+musimanager_directory = opts["paths"]["musimanager_directory"]
+
+newpipe_playlists = opts["newpipe"]["newpipe_playlists"]
+musitracker_search_limit = opts["ytmusic_search_limits"]["musitracker_search_limit"]
+musitracker_search_limit_first_time = opts["ytmusic_search_limits"]["musitracker_search_limit_first_time"]
+musi_download_ext = opts["music_formats"]["musi_download_ext"]
+search_exts = opts["music_formats"]["search_exts"]
+force_ascii_art = opts["ascii_art"]["force_ascii_art"]
+disable_ascii_art = opts["ascii_art"]["disable_ascii_art"]
+enable_global_shortcuts = opts["global_shortcuts"]["enable_global_shortcuts"]
+pause_global_shortcut = opts["global_shortcuts"]["pause_global_shortcut"]
+
+# random options
+show_artist_name_besides_song_name = opts["random_options"]["show_artist_name_besides_song_name"]
+show_hidden_in_file_explorer = opts["random_options"]["show_hidden_in_file_explorer"]
+save_on_exit = opts["random_options"]["save_on_exit"]
+auto_search_albums = opts["random_options"]["auto_search_albums"]
+debug_prints = opts["random_options"]["debug_prints"]
+
+
+
+
 
 newpipe_bkup_directory = musimanager_directory
-newpipe_playlists = [] # ['issac', 'sawitch']
-
 ytmusic_headers_path = musimanager_directory + "headers_auth.json"
 musitracker_path = musimanager_directory + "musitracker.json"
 temp_dir = musimanager_directory + ".temp/"
-musitracker_search_limit = 75
-musitracker_search_limit_first_time = 300
-
-musi_download_ext = "m4a"
-search_exts = ["mp3", "m4a", "flac", "ogg"]
-force_ascii_art = False # linux only (cuz theres ueberzug too) # pip install ueberzug
-disable_ascii_art = False
-
- # https://github.com/moses-palmer/pynput
-enable_global_shortcuts = False # pip install pynput
-pause_global_shortcut = "<cmd>+<F10>" # meta + f10
-
-
-
 
 default_album_art = musimanager_directory + "img.png"
 import platform
@@ -38,19 +69,10 @@ import ytmusicapi
 ytmusic = ytmusicapi.YTMusic(auth=ytmusic_headers_path)
 
 
-# random options
-show_artist_name_besides_song_name = True
-show_hidden_in_file_explorer = False
-save_on_exit = True
-auto_search_albums = False
-debug_prints = False
 
-
-
-import os
-if not all([os.path.exists(musimanager_directory), os.path.exists(auto_search_under), os.path.exists(file_explorer_base_dir)]):
-    print("instructions in README.md")
-    quit()
-else:
-    if not os.path.exists(temp_dir):
-        os.mkdir(temp_dir)
+for _, p in opts["paths"].items():
+    if not os.path.exists(p):
+        os.mkdir(p)
+        print("created directory: ", p)
+if not os.path.exists(temp_dir):
+    os.mkdir(temp_dir)
